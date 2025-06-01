5f34d0f5c872e283b2e4b97723b3b7e85a2964e6
CarranzaXav <carranzax7@gmail.com>
2025-06-01 16:05:42 -0400

    Backend Update

--- recipe-keeper-api/routes/recipeRoutes.js
+++ recipe-keeper-api/routes/recipeRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const recipesController = require("../controllers/recipesController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/")
