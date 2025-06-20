e8cea53056e1d6e44032f703cbecc7e0e4202762
CarranzaXav <carranzax7@gmail.com>
2025-06-19 23:48:50 -0400

    6/17/2025 Navbar Update

--- recipe-keeper/src/Components/Navbar.jsx
+++ recipe-keeper/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
 import useAuth from '../hooks/useAuth'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBurger} from '@fortawesome/free-solid-svg-icons'
 
@@ -11,7 +11,11 @@ const Navbar = () => {
   const [sendLogout, {isLoading, isSuccess, isError, error}] = useSendLogoutMutation()
   const [isHovered, setIsHovered] = useState(false)
 
-  const hoverClass = isHovered ? 'invisible' : 'duraction-300 ease-in-out text-purple-400'
+  useEffect (() => {
+      setIsHovered(false)
+  },[username])
+
+  const hoverClass = isHovered ? 'invisible' : 'duration-300 ease-in-out text-purple-400'
   const hiddenClass = isHovered ? '' : 'invisible'
 
   const hover = () => {
@@ -37,7 +41,39 @@ const Navbar = () => {
         </Link>
       </div>
 
-        {(!username) ?
+        {(username) ?
+          <nav className='navbarButtonContainer
+            w-2/10
+            ml-1/10
+            flex
+            justify-end
+            py-2
+            pr-[2%] hover:pr-0
+            cursor-pointer
+            bg-linear-270 from-[#b393cc] from-75% to-[var(--NAVBAR)]'
+            onMouseEnter={hover}
+            onMouseLeave={hover}
+          >
+          <div
+            className={`navbarLogoutBtn flex items-center font-semibold tracking-[2px] `}
+            title='Logout'
+            onClick={sendLogout}
+          >
+           <p className={`navBtn 
+              flex
+              ${hiddenClass}
+              text-white hover:text-purple-700
+              tracking-[2px]
+              font-semibold
+              `}>
+              Logout
+            </p>
+            <p className={`navBtnHolder text-4xl ${hoverClass}`}>
+              <FontAwesomeIcon icon={faBurger} style={{color: "white"}} />
+            </p>
+          </div> 
+        </nav>
+        :
           <nav className='navbarButtonContainer
             w-2/10
             ml-1/10 hover:ml-0
@@ -70,38 +106,7 @@ const Navbar = () => {
               </p>
             </Link>
           </nav> 
-        : 
-          <nav className='navbarButtonContainer
-            w-2/10
-            ml-1/10
-            flex
-            justify-end
-            py-2
-            pr-[2%] hover:pr-0
-            cursor-pointer
-            bg-linear-270 from-[#b393cc] from-75% to-[var(--NAVBAR)]'
-            onMouseEnter={hover}
-            onMouseLeave={hover}
-          >
-          <div
-            className={`navbarLogoutBtn flex items-center font-semibold tracking-[2px] `}
-            title='Logout'
-            onClick={sendLogout}
-          >
-           <p className={`navBtn 
-              flex
-              ${hiddenClass}
-              text-white hover:text-purple-700
-              tracking-[2px]
-              font-semibold
-              `}>
-              Logout
-            </p>
-            <p className={`navBtnHolder text-4xl ${hoverClass}`}>
-              <FontAwesomeIcon icon={faBurger} style={{color: "white",}} />
-            </p>
-          </div> 
-        </nav>
+
         }
     </header>  
 )
