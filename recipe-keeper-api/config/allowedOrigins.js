4cbb23b131f43e8ffee637b6f53cd522be2d8ca3
CarranzaXav <carranzax7@gmail.com>
2025-12-02 22:21:48 -0500

    API Update

--- recipe-keeper-api/config/allowedOrigins.js
+++ recipe-keeper-api/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   // For Production
   "http://localhost:3000",
   "https://recipe-keeper-ui.netlify.app",
+  "https://recipe-keeper-ui.netlify.app/",
   "https://xaviercarranza.com",
 
   //
